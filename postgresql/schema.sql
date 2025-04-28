58f576fbf36bf034232542841c13098d7d5fcffe
majid vayghani <mjvayghani@gmail.com>
2025-04-28 23:19:24 +0330

    refactor(schema): improve schema structure and naming conventions for PostgreSQL standards

--- postgresql/schema.sql
+++ postgresql/schema.sql
@@ -1,57 +1,80 @@
 -- postgresql/schema.sql
 -- SQL statements based on the 'Money-diagram.png'
 
-CREATE TYPE transaction_type AS ENUM ('INCOME', 'EXPENSE');
+CREATE TYPE transaction_category_enum AS ENUM ('income', 'expense');
 
--- Create the Users table
-CREATE TABLE Users (
-    Id UUID PRIMARY KEY,
-    UserName VARCHAR(20) UNIQUE NOT NULL,
-    Password UUID NOT NULL,
-    FirstName VARCHAR(20) NOT NULL,
-    LastName VARCHAR(40) NOT NULL,
-    Email VARCHAR(250) UNIQUE NOT NULL,
-    Is_Active BOOLEAN NOT NULL DEFAULT TRUE,
-    Is_Superuser BOOLEAN NOT NULL DEFAULT FALSE,
-    Created_at TIMESTAMP,
-    Updated_at TIMESTAMP,
-    SoftDeleted_at TIMESTAMP,
-    HardDeleted_at TIMESTAMP 
+CREATE TABLE accounts_user (
+    _id UUID PRIMARY KEY,
+    password VARCHAR(128) NOT NULL,
+    last_login TIMESTAMP WITH TIME ZONE NULL,
+    is_superuser BOOLEAN NOT NULL,
+    email VARCHAR(254) UNIQUE NOT NULL,
+    is_staff BOOLEAN NOT NULL,
+    is_active BOOLEAN NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    soft_deleted_at TIMESTAMP WITH TIME ZONE NULL,
+    hard_deleted_at TIMESTAMP WITH TIME ZONE NULL
 );
 
--- Create the Transactions table with a Foreign Key to Users
-CREATE TABLE Transactions (
-    Id UUID PRIMARY KEY,
-    _User UUID NOT NULL,
-    Type transaction_type NOT NULL, 
-    Date DATE NOT NULL,
-    Amount DECIMAL(11, 2) NOT NULL,
-    Category VARCHAR(25) NOT NULL,
-    Description VARCHAR(100) NULL,
-    Tag VARCHAR(15) NULL,
-    Created_at TIMESTAMP,
-    Updated_at TIMESTAMP,
-    SoftDeleted_at TIMESTAMP,
-    HardDeleted_at TIMESTAMP,
-
-    CONSTRAINT fk_user
-        FOREIGN KEY(_User)
-        REFERENCES Users(Id)
+CREATE TABLE accounts_profile (
+    _id UUID PRIMARY KEY,
+    _user_id UUID UNIQUE NOT NULL,
+    first_name VARCHAR(125) NOT NULL,
+    last_name VARCHAR(125) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    soft_deleted_at TIMESTAMP WITH TIME ZONE NULL,
+    hard_deleted_at TIMESTAMP WITH TIME ZONE NULL,
+    FOREIGN KEY (_user_id) REFERENCES accounts_user(_id) ON DELETE CASCADE
 );
 
--- Create the Token table with a Foreign Key to Users
-CREATE TABLE Token (
-    Id UUID PRIMARY KEY,
-    _User UUID NOT NULL,
-    Token VARCHAR(255) UNIQUE NOT NULL,
-    Is_Active BOOLEAN NOT NULL DEFAULT TRUE,
-    Created_at TIMESTAMP,
-    SoftDeleted_at TIMESTAMP,
-    HardDeleted_at TIMESTAMP,
-
-    CONSTRAINT fk_user
-        FOREIGN KEY(_User)
-        REFERENCES Users(Id)
+CREATE TABLE transactions_transactioncategory (
+    _id UUID PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    parent_id UUID NULL,
+    category_type transaction_category_enum NOT NULL DEFAULT 'expense',
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    soft_deleted_at TIMESTAMP WITH TIME ZONE NULL,
+    hard_deleted_at TIMESTAMP WITH TIME ZONE NULL,
+    FOREIGN KEY (parent_id) REFERENCES transactions_transactioncategory(_id) ON DELETE CASCADE
 );
 
--- Optional: Add indexes for frequently queried columns, especially Foreign Keys
+-- index for the parent_id field to improve performance in tree-based queries
+CREATE INDEX idx_category_parent_id ON transactions_transactioncategory (parent_id);
+
+CREATE TABLE transactions_transaction (
+    _id UUID PRIMARY KEY,
+    _user_id UUID NOT NULL,
+    date TIMESTAMP WITH TIME ZONE NOT NULL,
+    amount NUMERIC(10, 2) NOT NULL,
+    _category_id UUID NULL,
+    description TEXT NOT NULL,
+    tag VARCHAR(50) NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    soft_deleted_at TIMESTAMP WITH TIME ZONE NULL,
+    hard_deleted_at TIMESTAMP WITH TIME ZONE NULL,
+    FOREIGN KEY (_user_id) REFERENCES accounts_user(_id) ON DELETE CASCADE,
+    FOREIGN KEY (_category_id) REFERENCES transactions_transactioncategory(_id) ON DELETE SET NULL
+);
+
+-- index for the _user_id field to improve performance in queries for user transactions
+CREATE INDEX idx_transaction_user_id ON transactions_transaction (_user_id);
+
+-- index for the _category_id field to improve performance in queries for transactions by category
+CREATE INDEX idx_transaction_category_id ON transactions_transaction (_category_id);
+
+CREATE TABLE tokens_token (
+    _id UUID PRIMARY KEY,
+    _user_id UUID NOT NULL,
+    is_active BOOLEAN NOT NULL,
+    token VARCHAR(255) UNIQUE NOT NULL,
+    expired_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    soft_deleted_at TIMESTAMP WITH TIME ZONE NULL,
+    hard_deleted_at TIMESTAMP WITH TIME ZONE NULL,
+    FOREIGN KEY (_user_id) REFERENCES accounts_user(_id) ON DELETE CASCADE
+);
